54b5c265936322436e51e0dd3f3cc49ab65172c9
nikhil bagal <nbagal02@gmail.com>
2019-10-21 13:33:11 +0530

    final layout and functionality built

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-var list = []
-    /*1 : 'eat',
-    2 : 'repeat',*/
-
+var list = [];
 var element = document.querySelector('.task-view');
 var inputElement = document.querySelector('.form-sec form input');
-/*console.log(inputElement.value);
-function update(){
-    /*if(list.length == 0){*/
-      /*  var inputElement = document.querySelector('.form-sec form input');
-        list.push(inputElement);
-        console.log(inputElement);*/
-   // }
-   
- /*  list.push(inputElement.value);
-}
-console.log(list[0]);*/
-
-
 function update(){
   event.preventDefault();
   var val = inputElement.value;
   list.push(val);
-  element.innerHTML += "<p>"+list[list.length-1]+"</p>"+"<input type='checkBox'>";
+  //element.innerHTML += "<p>"+list[list.length-1]+"</p>"+"<input type='checkBox'>";
+  element.innerHTML += "<div class='tasks'>"+"<p>"+list[list.length-1]+"</p>"+
+  "<input type='checkbox' name='chk'>"+"<span>"+
+  "<i class='fa fa-trash' aria-hidden='true'></i>"+"</span>"+"<span class='line'><hr></span>";
+  del();
 }
-
+function del(){
+  var element = document.querySelectorAll(".task-view .tasks span i");
+  for(i of element){
+      i.addEventListener('click',function(){
+      var selectedElement = event.target;
+      selectedElement.parentNode.parentNode.classList.add("deleted");
+    })
+  }
+  
+ // console.log(element);
+}
