cc622c9bbdca8e20ccc210f964754de2288bc475
nikhil bagal <nbagal02@gmail.com>
2019-10-22 14:40:41 +0530

    bug resolved in js code

--- script.js
+++ script.js
@@ -6,12 +6,37 @@ function update(){
   var val = inputElement.value;
   list.push(val);
   inputElement.value = "";
+  var innerEle = addElement('div','tasks');
   if(val !== ""){
-    element.innerHTML += "<div class='tasks' id="+list.indexOf(val)+">"+"<p>"+list[list.length-1]+"</p>"+
-  "<input type='checkbox' name='chk'>"+"<span>"+
-  "<i class='fa fa-trash' aria-hidden='true'></i>"+"</span>"+"<span class='line'><hr></span>";
+    element.appendChild(innerEle);
   }
   del();
+}
+function addElement(elementTag,elementClass){
+  var newElement = document.createElement(elementTag);
+  newElement.className = elementClass;
+  var all = innerChilds();
+  newElement.appendChild(all[0]);
+  newElement.appendChild(all[1]);
+  newElement.appendChild(all[2]);
+  newElement.appendChild(all[3]);
+  return newElement;
+}
+function innerChilds(){
+  var para = document.createElement('p');
+  para.innerHTML = list[list.length-1];
+  var input = document.createElement('INPUT');
+  input.setAttribute('type','checkbox');
+  var span = document.createElement('span');
+  var icon = document.createElement('i');
+  icon.className = 'fa fa-trash';
+  icon.setAttribute('aria-hidde',true);
+  span.appendChild(icon);
+  var line = document.createElement('span');
+  line.className = 'line';
+  line.innerHTML += '<hr>';
+  var arr = [para,input,span,line]
+  return arr;
 }
 function del(){
   var element = document.querySelectorAll(".task-view .tasks span i");
